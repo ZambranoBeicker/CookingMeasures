f443144bcecaf13c3fad2c629baea6e8193d295f
Beicker Zambrano <beicker998@gmail.com>
2021-10-31 00:15:05 -0500

    Create a styles file for TimerSection

---
+++ src/components/TimerSection/styles.ts
@@ -0,0 +1,24 @@
+import {ViewStyle, TextStyle} from 'react-native';
+
+export const container: ViewStyle = {
+  borderBottomColor: '#0002',
+  borderBottomWidth: 1,
+
+  padding: 8,
+};
+
+export const timerText: TextStyle = {
+  fontSize: 18,
+  fontWeight: 'bold',
+  textAlign: 'center',
+};
+
+export const titleContainer: ViewStyle = {
+  marginBottom: 20,
+};
+
+export const title: TextStyle = {
+  fontSize: 24,
+  fontWeight: 'bold',
+  textAlign: 'center',
+};
