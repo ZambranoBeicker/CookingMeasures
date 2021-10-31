910e73ee7dbff41b262f34c424bea32b007ff08b
Beicker Zambrano <beicker998@gmail.com>
2021-10-31 14:07:38 -0500

    Create styles file for the AddButton component

---
+++ src/components/AddButton/styles.ts
@@ -0,0 +1,31 @@
+import {ViewStyle, TextStyle} from 'react-native';
+
+const commonStyles: ViewStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+};
+
+const buttonSize: number = 50;
+
+export const container: ViewStyle = {
+  paddingVertical: 8,
+  position: 'absolute',
+  width: '100%',
+  bottom: 0,
+  ...commonStyles,
+};
+
+export const button: ViewStyle = {
+  backgroundColor: 'black',
+  width: buttonSize,
+  height: buttonSize,
+  paddingTop: 2,
+  borderRadius: buttonSize,
+  ...commonStyles,
+};
+
+export const text: TextStyle = {
+  fontSize: 32,
+  color: 'white',
+};
