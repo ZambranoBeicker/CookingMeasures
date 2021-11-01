0d78282e8d5f1afd0fe9b3c9445e45c3de93d64c
Beicker Zambrano <beicker998@gmail.com>
2021-10-31 19:09:52 -0500

    Add props to change state on pressed

--- src/components/AddButton/index.tsx
+++ src/components/AddButton/index.tsx
@@ -2,10 +2,14 @@ import * as React from 'react';
 import {Pressable, Text, View} from 'react-native/';
 import * as styles from './styles';
 
-export default function AddButton(): JSX.Element {
+type AddButtonProps = {
+  setClicked: (newValue: boolean) => void;
+};
+
+export default function AddButton({setClicked}: AddButtonProps): JSX.Element {
   return (
     <View style={styles.container}>
-      <Pressable style={styles.button}>
+      <Pressable onPress={() => setClicked(true)} style={styles.button}>
         <Text style={styles.text}>+</Text>
       </Pressable>
     </View>
