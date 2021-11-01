7ede5f1f65e53ae91a121aaabe411a72a0886d55
Beicker Zambrano <beicker998@gmail.com>
2021-10-31 19:34:41 -0500

    Add props to show a list of items dynamically

--- src/components/StepsList/index.tsx
+++ src/components/StepsList/index.tsx
@@ -2,7 +2,15 @@ import * as React from 'react';
 import {Text, View} from 'react-native/';
 import * as styles from './styles';
 
-export default function AddButton(): JSX.Element {
+interface list {
+  name: string;
+}
+
+type StepListProps = {
+  list: list[];
+};
+
+export default function StepList({list}: StepListProps): JSX.Element {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Your steps list</Text>
