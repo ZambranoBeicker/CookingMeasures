b4b486479d387b9f98182e228aa8d3feed0f9c70
Beicker Zambrano <beicker998@gmail.com>
2021-11-03 15:35:51 -0500

    Manage the layout selection

--- App.tsx
+++ App.tsx
@@ -11,17 +11,32 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {View, Text} from 'react-native';
+import Default from './src/layout/Default';
 import Timer from './src/screens/Timer';
 import Home from './src/screens/Home';
 
 const App = () => {
   const Stack = createNativeStackNavigator();
 
+  const screens: {[name: string]: () => JSX.Element} = {
+    Home: () => (
+      <Default>
+        <Home />
+      </Default>
+    ),
+    Timer: () => (
+      <Default>
+        <Home />
+      </Default>
+    ),
+  };
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Timer" component={Timer} />
+        <Stack.Screen name="Home" component={screens.Home} />
+        <Stack.Screen name="Timer" component={screens.Timer} />
       </Stack.Navigator>
     </NavigationContainer>
   );
