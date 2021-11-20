2c691e70034ea94267194cce1666eecd8861be9c
Beicker Zambrano <beicker998@gmail.com>
2021-11-20 08:38:35 -0500

    Create reusable Button component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,12 @@
+import {ViewStyle, TextStyle} from 'react-native';
+
+export const button: ViewStyle = {
+  paddingVertical: 8,
+  paddingHorizontal: 20,
+  marginHorizontal: 4,
+  borderRadius: 4,
+};
+
+export const buttonText: TextStyle = {
+  color: 'white',
+};
