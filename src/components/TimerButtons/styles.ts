06bb424060e24767105938676ce77a62cc6d673a
Beicker Zambrano <beicker998@gmail.com>
2021-11-20 08:38:07 -0500

    Create buttons to control timer

---
+++ src/components/TimerButtons/styles.ts
@@ -0,0 +1,8 @@
+import {ViewStyle, TextStyle} from 'react-native';
+
+export const container: ViewStyle = {
+  paddingVertical: 8,
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+};
