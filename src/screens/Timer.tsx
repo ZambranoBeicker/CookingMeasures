106f6d52eebaeb0b84944e39a04978242e9ced78
Beicker Zambrano <beicker998@gmail.com>
2021-11-20 08:39:30 -0500

    Add TimerButtons into Timer screen
    Add the needed components with the corresponding states to pass them via
    props. Wrap StepsList and TimerButtons components inside a flex
    container for them to be separated each other
    
    Finally tweak StepsList styles for its height to cover only the
    necessary space

--- src/screens/Timer.tsx
+++ src/screens/Timer.tsx
@@ -3,6 +3,7 @@ import {useState} from 'react';
 import {View, ViewStyle} from 'react-native';
 import TimerSection from '../components/TimerSection';
 import StepsList from '../components/StepsList';
+import TimerButtons from '../components/TimerButtons';
 
 const homeStyles: ViewStyle = {
   paddingHorizontal: 20,
@@ -11,11 +12,30 @@ const homeStyles: ViewStyle = {
 
 export default function Timer() {
   const [time, setTime] = useState(0);
+  const [pause, setPause] = useState(false);
+  const [start, setStart] = useState(false);
+  const [finish, setFinish] = useState(false);
+
+  const tasksContainerStyles: ViewStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    height: '86%',
+  };
   return (
     <View style={{flex: 1}}>
       <View style={homeStyles}>
         <TimerSection time={time} />
-        <StepsList list={[{name: ''}]} />
+        <View style={{display: 'flex', flexDirection: 'column'}}>
+          <View style={tasksContainerStyles}>
+            <StepsList list={[{name: ''}]} />
+            <TimerButtons
+              setPause={() => setPause(!pause)}
+              setStart={() => setStart(!start)}
+              setFinish={() => setFinish(!finish)}
+            />
+          </View>
+        </View>
       </View>
     </View>
   );
