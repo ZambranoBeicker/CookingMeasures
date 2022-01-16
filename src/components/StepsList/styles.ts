49b8f4927da04fba582a72b312bc38c2bc375aec
Beicker Zambrano <beicker998@gmail.com>
2022-01-16 12:45:28 -0500

    Delete not used styles

--- src/components/StepsList/styles.ts
+++ src/components/StepsList/styles.ts
@@ -1,34 +1,13 @@
 import {ViewStyle, TextStyle} from 'react-native';
 
-const buttonSize: number = 50;
-
 export const container: ViewStyle = {
   padding: 8,
   maxHeight: '85%',
 };
 
-export const listContainer: ViewStyle = {
-  paddingVertical: 8,
-  backgroundColor: 'white',
-  borderRadius: 4,
-};
-
-export const listItem: ViewStyle = {
-  paddingHorizontal: 16,
-  paddingVertical: 8,
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-  borderBottomWidth: 1,
-  borderColor: '#6F6F6F',
-};
 export const title: TextStyle = {
   fontSize: 24,
   fontWeight: 'bold',
   textAlign: 'center',
   marginBottom: 12,
-};
-
-export const itemText: TextStyle = {
-  fontSize: 18,
 };
